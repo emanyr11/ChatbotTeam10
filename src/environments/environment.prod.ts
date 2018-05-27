07998361382e83e04c47edc026bbb1e518b2fda8
Emanyr11 <emanyr11@gmail.com>
2018-05-27 15:51:52 +1200

    environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  dialogflow: {
+    ChatbotTeam10: 'de2a3c3adb8544ceb13d146c2d6f64ed'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+
+  dialogflow: {
+    ChatbotTeam10: 'de2a3c3adb8544ceb13d146c2d6f64ed'
+  }
+};
